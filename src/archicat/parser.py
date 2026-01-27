0ca9b4e83dbde84fd0fa6e96a9c84e0db3f82758
jasper-91 <jasper.liese@gmail.com>
2026-01-27 15:50:22 +0100

    added grammar to package data

--- src/archicat/parser.py
+++ src/archicat/parser.py
@@ -3,10 +3,10 @@ from .error import ArchiCatSyntaxError
 from lark import Lark,Tree
 from lark.exceptions import UnexpectedEOF,UnexpectedCharacters,UnexpectedToken
 from pathlib import Path
+from importlib.resources import files
 
 
-with open(Path(__file__).parent / 'grammar.lark') as file:
-    parser = Lark(file.read())
+parser = Lark(files('archicat').joinpath('grammar.lark').read_text())
 
 def parse(text: str) -> Tree:
     try:
